a05edf3873fb42b71a6f4c01112162e401d52214
Suhas <suhas.shrestha1@gmail.com>
2016-11-23 11:06:38 +0700

    new update

--- lib/generators/templates/controllers/graphgraphs_controller.rb
+++ lib/generators/templates/controllers/graphgraphs_controller.rb
@@ -1,140 +1,51 @@
 class <%= @scope_prefix %>GraphgraphsController < ApplicationController
 	def graphgraph
+
 		@scopeName= <%= @scope.capitalize %>
 		@testData= <%= @scope.capitalize %>.all
 		@columnNames = <%= @scope.capitalize %>.column_names
-
-		@columnHash = <%= @scope.capitalize %>.columns_hash.each {|k,v| puts "#{k} => #{v.type}"}
+		@columnHash = %= @scope.capitalize %>.columns_hash.each {|k,v| puts "#{k} => #{v.type}"}
 		@selectoptions =[]
 		@columnNames.each do 	|c|
 			@selectoptions << c.to_s + " : " + @columnHash[c].type.to_s
 		end
+		call_counts
+		set_graph
+		set_user_template
+		puts @foruser
+		set_bargraph
+		set_linegraph
+		set_areagraph
+		set_scattergraph
+
+		if params[:fromuser]=="fromuser"
+			call_counts
+			set_graph
+			set_user_template
+			puts @foruser
+			set_bargraph
+			set_linegraph
+			set_areagraph
+			set_scattergraph
+			@mainchart=@chartline||@chartbar||@chartarea||@chartscatter
+			redirect_to <%= @scope.capitalize %>s_path(options: @mainchart.objects.to_s, placeholder: @mainchart.placeholder.to_s, series_data: @mainchart.series_data.to_s)
+		end
+	end
+
+	private
+	def call_counts
 		if (params[:xaxis]!=nil)
 			if params[:xaxis].include? "datetime"
 				get_counts(params[:xaxis],params[:yaxis],params[:xstart].split(/\s+/),params[:xend].split(/\s+/))
 			end
 		end
+	end
+
+	def set_graph
 		@graphtype=["Bar Graph", "Line Graph", "Area Graph", "Scatter Graph"]
-		if (params[:graphtype].to_s == "Bar Graph")
+	end
 
-			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			tempx = params[:xaxis].slice(0..(params[:xaxis].index(' '))) # determine user-given variable name
-			final_x = tempx.strip
-			tempy = params[:yaxis].slice(0..(params[:yaxis].index(' '))) # determine user-given variable name
-			final_y = tempy.strip
-			# instance_variable_set("@#{variable_name}", params[:xaxis])
-			# puts <%= @scope.capitalize %>.pluck(final_x)
-			@barcat=[]
-			@bardata=[]
-			@chartbar = LazyHighCharts::HighChart.new('column') do |f|
-						if (@x_ticks!=nil)
-							@x_ticks.each do |x|
-								@barcat << x[0]
-								@bardata << x[1]
-							end
-							f.xAxis(:categories => @barcat)
-							f.series(:data=> @bardata)
-						else
-							f.xAxis(:categories => <%= @scope.capitalize %>.pluck(final_x))
-							f.series(:data=> <%= @scope.capitalize %>.pluck(final_y))
-						end
-
-		        # f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )
-		        f.title({ :text=>params[:title].to_s})
-		        f.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
-
-
-		        ### Options for Bar
-		        ### f.options[:chart][:defaultSeriesType] = "bar"
-		        ### f.plot_options({:series=>{:stacking=>"normal"}})
-
-		        ## or options for column
-		        f.options[:chart][:defaultSeriesType] = "column"
-		        # f.plot_options({:column=>{:stacking=>"percent"}})
-		    end
-		end
-
-		if (params[:graphtype].to_s == "Line Graph")
-
-			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			tempx = params[:xaxis].slice(0..(params[:xaxis].index(' '))) # determine user-given variable name
-			final_x = tempx.strip
-			tempy = params[:yaxis].slice(0..(params[:yaxis].index(' '))) # determine user-given variable name
-			final_y = tempy.strip
-			# instance_variable_set("@#{variable_name}", params[:xaxis])
-			# puts <%= @scope.capitalize %>.pluck(final_x)
-			@linecat=[]
-			@linedata=[]
-			@chartline = LazyHighCharts::HighChart.new('line') do |f|
-					if (@x_ticks!=nil)
-						@x_ticks.each do |x|
-							@linecat << x[0]
-							@linedata << x[1]
-						end
-						f.xAxis(:categories => @linecat)
-						f.series(:data=> @linedata)
-					else
-						f.xAxis(:categories => <%= @scope.capitalize %>.pluck(final_x))
-						f.series(:data=> <%= @scope.capitalize %>.pluck(final_y))
-					end
-		        # f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )
-		        f.title({ :text=>params[:title].to_s})
-		        f.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
-
-
-		        ### Options for Bar
-		        ### f.options[:chart][:defaultSeriesType] = "bar"
-		        ### f.plot_options({:series=>{:stacking=>"normal"}})
-
-		        ## or options for column
-		        # f.options[:chart][:defaultSeriesType] = "column"
-		        # f.plot_options({:column=>{:stacking=>"percent"}})
-		    end
-		end
-
-		if (params[:graphtype].to_s == "Area Graph")
-
-			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-			tempx = params[:xaxis].slice(0..(params[:xaxis].index(' '))) # determine user-given variable name
-			final_x = tempx.strip
-			tempy = params[:yaxis].slice(0..(params[:yaxis].index(' '))) # determine user-given variable name
-			final_y = tempy.strip
-			# instance_variable_set("@#{variable_name}", params[:xaxis])
-			# puts <%= @scope.capitalize %>.pluck(final_x)
-			@areacat=[]
-			@areadata=[]
-			@chartarea = LazyHighCharts::HighChart.new('area') do |f2|
-				if (@x_ticks!=nil)
-					@x_ticks.each do |x|
-						@areacat << x[0]
-						@areadata << x[1]
-					end
-					f2.xAxis(:categories => @areacat)
-					f2.series(:data=> @areadata)
-				else
-					f2.xAxis(:categories => <%= @scope.capitalize %>.pluck(final_x))
-					f2.series(:data=> <%= @scope.capitalize %>.pluck(final_y))
-				end
-        # f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )
-        f2.title({ :text=>params[:title].to_s})
-        f2.plot_options(series: {
-                            stacking:'normal'
-                        })
-				f2.chart({ defaultSeriesType: 'area'})
-        f2.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
-
-
-        ### Options for Bar
-        ### f.options[:chart][:defaultSeriesType] = "bar"
-        ### f.plot_options({:series=>{:stacking=>"normal"}})
-
-        ## or options for column
-        # f.options[:chart][:defaultSeriesType] = "column"
-        # f.plot_options({:column=>{:stacking=>"percent"}})
-
-    	end
-		end
-
+	def set_scattergraph
 		if (params[:graphtype].to_s == "Scatter Graph")
 
 			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -179,7 +90,190 @@ class <%= @scope_prefix %>GraphgraphsController < ApplicationController
 		end
 	end
 
-	private
+	def set_areagraph
+		if (params[:graphtype].to_s == "Area Graph")
+
+			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			tempx = params[:xaxis].slice(0..(params[:xaxis].index(' '))) # determine user-given variable name
+			final_x = tempx.strip
+			tempy = params[:yaxis].slice(0..(params[:yaxis].index(' '))) # determine user-given variable name
+			final_y = tempy.strip
+			# instance_variable_set("@#{variable_name}", params[:xaxis])
+			# puts <%= @scope.capitalize %>.pluck(final_x)
+			@areacat=[]
+			@areadata=[]
+			@chartarea = LazyHighCharts::HighChart.new('area') do |f2|
+				if (@x_ticks!=nil)
+					@x_ticks.each do |x|
+						@areacat << x[0]
+						@areadata << x[1]
+					end
+					f2.xAxis(:categories => @areacat)
+					f2.series(:data=> @areadata)
+				else
+					f2.xAxis(:categories => <%= @scope.capitalize %>.pluck(final_x))
+					f2.series(:data=> <%= @scope.capitalize %>.pluck(final_y))
+				end
+        # f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )
+        f2.title({ :text=>params[:title].to_s})
+        f2.plot_options(series: {
+                            stacking:'normal'
+                        })
+				f2.chart({ defaultSeriesType: 'area'})
+        f2.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
+
+
+        ### Options for Bar
+        ### f.options[:chart][:defaultSeriesType] = "bar"
+        ### f.plot_options({:series=>{:stacking=>"normal"}})
+
+        ## or options for column
+        # f.options[:chart][:defaultSeriesType] = "column"
+        # f.plot_options({:column=>{:stacking=>"percent"}})
+
+    	end
+		end
+	end
+
+	def set_linegraph
+		if (params[:graphtype].to_s == "Line Graph")
+
+			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			tempx = params[:xaxis].slice(0..(params[:xaxis].index(' '))) # determine user-given variable name
+			final_x = tempx.strip
+			tempy = params[:yaxis].slice(0..(params[:yaxis].index(' '))) # determine user-given variable name
+			final_y = tempy.strip
+			# instance_variable_set("@#{variable_name}", params[:xaxis])
+			# puts <%= @scope.capitalize %>.pluck(final_x)
+			@linecat=[]
+			@linedata=[]
+			@chartline = LazyHighCharts::HighChart.new('line') do |f|
+					if (@x_ticks!=nil)
+						@x_ticks.each do |x|
+							@linecat << x[0]
+							@linedata << x[1]
+						end
+						f.xAxis(:categories => @linecat)
+						f.series(:data=> @linedata)
+					else
+						f.xAxis(:categories => <%= @scope.capitalize %>.pluck(final_x))
+						f.series(:data=> <%= @scope.capitalize %>.pluck(final_y))
+					end
+		        # f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )
+		        f.title({ :text=>params[:title].to_s})
+		        f.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
+
+
+		        ### Options for Bar
+		        ### f.options[:chart][:defaultSeriesType] = "bar"
+		        ### f.plot_options({:series=>{:stacking=>"normal"}})
+
+		        ## or options for column
+		        # f.options[:chart][:defaultSeriesType] = "column"
+		        # f.plot_options({:column=>{:stacking=>"percent"}})
+		    end
+		end
+	end
+
+	def set_bargraph
+		if (params[:graphtype].to_s == "Bar Graph")
+
+			# puts "import !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+			tempx = params[:xaxis].slice(0..(params[:xaxis].index(' '))) # determine user-given variable name
+			final_x = tempx.strip
+			tempy = params[:yaxis].slice(0..(params[:yaxis].index(' '))) # determine user-given variable name
+			final_y = tempy.strip
+			# instance_variable_set("@#{variable_name}", params[:xaxis])
+			# puts <%= @scope.capitalize %>.pluck(final_x)
+			@barcat=[]
+			@bardata=[]
+			@chartbar = LazyHighCharts::HighChart.new('column') do |f|
+						if (@x_ticks!=nil)
+							@x_ticks.each do |x|
+								@barcat << x[0]
+								@bardata << x[1]
+							end
+							f.xAxis(:categories => @barcat)
+							f.series(:data=> @bardata)
+						else
+							f.xAxis(:categories => <%= @scope.capitalize %>.pluck(final_x))
+							f.series(:data=> <%= @scope.capitalize %>.pluck(final_y))
+						end
+
+						# f.series(:name=>'Jane',:data=>[1, 3, 4, 3, 3, 5, 4,-46] )
+						f.title({ :text=>params[:title].to_s})
+						f.legend(:layout=> 'horizontal',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
+
+
+						### Options for Bar
+						### f.options[:chart][:defaultSeriesType] = "bar"
+						### f.plot_options({:series=>{:stacking=>"normal"}})
+
+						## or options for column
+						f.options[:chart][:defaultSeriesType] = "column"
+						# f.plot_options({:column=>{:stacking=>"percent"}})
+				end
+		end
+	end
+
+	def set_user_template
+		current_model_path=<%= @scope.capitalize %>.to_s.downcase+"s"
+		@foruser="<%= form_tag(\"/#{current_model_path}/graphgraph\", method: \"get\") do %>
+		<input type=\"text\" name=\"graphtype\" id=\"graphtype\" value=\"#{params[:graphtype]}\" hidden>
+		<input type=\"text\" name=\"yaxis\" id=\"yaxis\" value=\"#{params[:yaxis]}\" hidden>
+		<input type=\"text\" name=\"xaxis\" id=\"xaxis\" value=\"#{params[:xaxis]}\" hidden>
+		<input type=\"text\" name=\"fromuser\" id=\"fromuser\" value=\"fromuser\" hidden>
+
+	  <div class='col-sm-2' id=\"datepickerforxview\" hidden>
+	      <input type='text' name=\"xstart\" class=\"form-control\" id='datetimepicker1view' />
+	      to
+	      <input type='text' name=\"xend\" class=\"form-control\" id='datetimepicker2view' /></br>
+	      <input type=\"radio\" name=\"rangetypex\" value=\"minute\"> per Minute<br>
+	      <input type=\"radio\" name=\"rangetypex\" value=\"hourly\" checked> Hourly<br>
+	      <input type=\"radio\" name=\"rangetypex\" value=\"daily\"> Daily<br>
+	      <input type=\"radio\" name=\"rangetypex\" value=\"monthly\"> Monthly<br>
+	  </div>
+		<%= submit_tag 'Generate Graph', :id=>\"submitselection\"%>
+		<% end %>
+		<script>
+		  $(function () {
+		    $('#datetimepicker1view').datetimepicker();
+		    $('#datetimepicker2view').datetimepicker({
+		        useCurrent: false //Important! See issue #1075
+		    });
+		    $(\"#datetimepicker1view\").on(\"dp.change\", function (e) {
+		        $('#datetimepicker2view').data(\"DateTimePicker\").minDate(e.date);
+		    });
+		    $(\"#datetimepicker2view\").on(\"dp.change\", function (e) {
+		        $('#datetimepicker1view').data(\"DateTimePicker\").maxDate(e.date);
+		    });
+			});
+			$(document).ready(function() {
+        if (document.getElementById('xaxis').value.indexOf('datetime')!=-1)
+          document.getElementById('datepickerforxview').style.display= \"inline\";
+        else
+          document.getElementById('datepickerforxview').style.display= \"none\";
+      });
+		</script>
+		<% if params[:options]!=nil %>
+			<% newchart=LazyHighCharts::HighChart.new() %>
+			<% newchart.options=eval(params[:options]) %>
+			<% newchart.placeholder=params[:placeholder] %>
+			<% newchart.series_data=eval(params[:series_data]) %>
+			<% if (\"#{params[:graphtype]}\"==\"Bar Graph\") %>
+			    <%= high_chart(\"my_bar_div\", newchart) %>
+			   <% end %>
+			<% if (\"#{params[:graphtype]}\"==\"Line Graph\") %>
+			    <%= high_chart(\"my_line_div\", newchart) %>
+			<% end %>
+			<% if (\"#{params[:graphtype]}\"==\"Area Graph\") %>
+			    <%= high_chart(\"my_area_div\", newchart) %>
+			<% end %>
+			<% if (\"#{params[:graphtype]}\"==\"Scatter Graph\") %>
+			    <%= high_chart(\"my_scatter_div\", newchart) %>
+			<% end %>
+		<% end %>"
+	end
 
 	def get_counts(dateValue,pluckValue,startvalue,endvalue)
 	@newx= startvalue
@@ -238,8 +332,16 @@ class <%= @scope_prefix %>GraphgraphsController < ApplicationController
 			end
 			# @groups = [:camera_id, "year(#{ts_conversion})", "month(#{ts_conversion})"]
 	end
-	end
-	# def testMethod
-	# 	super
+
+
+	# @counts = LicensePlate.
+	# 		where(camera_id: @cameras.map { |cam| cam.id }).
+	# 		where('timestamp >= ?', @begin_date).
+	# 		where('timestamp <= ?', @end_date).
+	# 		where('license_id like ?', "%#{@plate_query}%").
+	# 		group(@groups).count
+	# @series = @cameras.map do |cam|
+	# 	{ name: cam.name, yAxis: 0, data: @x_ticks.map { |xtick| @counts[counts_index(cam, xtick)] || 0 } }
 	# end
+end
 end
